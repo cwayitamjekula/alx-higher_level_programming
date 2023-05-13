b53d57efce12eecc445e05f2d7d00f8c687aafbc
cwayitamjekula <cwayitamjekula@gmail.com>
2023-05-12 22:15:49 -0700

    made correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """print different combos of 2 digits"""
 
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
+for number in range(0, 10):
+    for number2 in range(number+1, 10):
+        if number ==8 and number2 ==9:
+            print("{:d}{:d}".format(number, number2))
+        else:
+            print("{:d}{:d}".format(number.number2), end=", ")
