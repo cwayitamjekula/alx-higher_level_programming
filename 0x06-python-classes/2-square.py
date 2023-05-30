6debac353b617d464c7daf21286365595be30daa
cwayitamjekula <cwayitamjekula@gmail.com>
2023-05-31 01:07:33 +0200

    fixed errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
+ """ square class """
 class Square():
     """ size set as a private attribute """
     def __init__(self, size=0):
         """ size: size of the square """
+       self.__size = size
         if type(size) is not int:
             raise TypeError("make sure size is an integer")
         if size < 0:
             raise ValueError("size can not be a negative number")
-        self.__size = size
