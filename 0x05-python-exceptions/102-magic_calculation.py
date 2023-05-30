d2f55d0e377686fe81199292f1f9992639423217
cwayitamjekula <cwayitamjekula@gmail.com>
2023-05-30 04:36:38 +0200

    fixed error

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    count = 0
+    result = 0
     for i in range(1, 3):
 
         try:
             if i > a:
                 raise Exception("Too far")
-            count += (a ** b) / i
+            result += (a ** b) / i
         except Exception:
-            count = b + a
+            result = b + a
             break
-        return count
+        return result
